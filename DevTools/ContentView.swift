9bc2e0f37dd1e1d21131735188e6c8d4a1a70077
uncleflower <jiehaozhang17@gmail.com>
2024-09-22 00:58:22 +0800

    Initial Commit

---
+++ DevTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevTools
+//
+//  Created by Karen on 2024/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevTools/DevToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevToolsApp.swift
+//  DevTools
+//
+//  Created by Karen on 2024/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DevToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
